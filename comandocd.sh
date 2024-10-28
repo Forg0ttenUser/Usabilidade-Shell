582a8ce62744d456b6aa1730ae2688a0e729e155
AzuosShazous <lucasdasghagasdesouza@gmail.com>
2024-10-28 20:28:15 -0300

    Cria arquivo de usos do comando cd

---
+++ comandocd.sh
@@ -0,0 +1,21 @@
+# Para trocar entre diretórios no seu sistema de arquivos, utilize o comando interno "cd" (sem aspas)
+
+cd /caminho/absoluto/do/diretorio
+
+# Acima está um exemplo, que muda para um diretório a partir do caminho absoluto (utilização do diretório raiz (/))
+
+cd ./caminho/do/diretorio
+
+# Acima está um exemplo, que muda para um diretório a partir do caminho relativo ao diretório de trabalho atual (./)
+
+cd ~
+
+# Em sistemas de base Unix, o "~" representa/substitui o diretório usuário do diretório home. O exemplo acima muda para o diretório usuário do diretório home
+
+cd ..
+
+# O exemplo acima muda para o diretório pai do diretório atual (./)
+
+cd
+
+# O exemplo acima tem o mesmo efeito do comando cd ~
