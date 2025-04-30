ff6f7e64614374df2b20a989601d96f06c87e7ec
Forg0ttenUser <lucasdasghagasdesouza@gmail.com>
2025-04-30 08:55:11 -0300

    Estiliza infos.sh

--- infos.sh
+++ infos.sh
@@ -2,12 +2,18 @@
 clear
 
 echo "Informações de sistema:"
-
+echo ----------------------------
 #Nome do Usuário corrente.
+echo -n "Usuário: "
 whoami
 #Nome do computador na rede.
+echo -n "Host: "
 hostname
 #Tempo de atividade do sistema computacional.
+echo -n "Tempo: "
 uptime -p
 #Informações de Hardware e Kernel.
+echo -n "Kernel: "
 uname -rms
+echo ----------------------------
+echo
